16d7002cb76b0dd5697cf88e523941ab9d64d10e
motijirata <jirata52@gmail.com>
2025-06-09 19:17:29 +0200

    Upadate server

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-const express = require("express");
-const next = require("next");
-
-const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const { createServer } = require('http');
+const next = require('next');
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
-
 app.prepare().then(() => {
-  const server = express();
-
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(process.env.PORT || 3000);
 });
